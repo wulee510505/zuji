4da4ed0022a1b839349ad0f80e7c13ac918f2135
wulee510505 <wulee510505@gmail.com>
2017-09-05 16:00:43 +0800

    添加 笑话/趣图 功能

--- app/src/main/java/com/wulee/administrator/zuji/ui/JokeActivity.java
+++ app/src/main/java/com/wulee/administrator/zuji/ui/JokeActivity.java
@@ -212,14 +212,16 @@ public class JokeActivity extends BaseActivity {
 
     private List<FunPicInfo>  processJokePicInfo(List<JokeInfo> list){
         List<FunPicInfo> jokePicDatas = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            JokeInfo joke  = list.get(i);
+        if(list != null && list.size()>0){
+            for (int i = 0; i < list.size(); i++) {
+                JokeInfo joke  = list.get(i);
 
-            FunPicInfo picInfo = new FunPicInfo();
-            picInfo.setUrl(joke.getUrl());
-            picInfo.set_id(joke.getHashId());
+                FunPicInfo picInfo = new FunPicInfo();
+                picInfo.setUrl(joke.getUrl());
+                picInfo.set_id(joke.getHashId());
 
-            jokePicDatas.add(picInfo);
+                jokePicDatas.add(picInfo);
+            }
         }
         return  jokePicDatas;
     }
